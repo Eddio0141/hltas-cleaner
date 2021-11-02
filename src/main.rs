1646b818b526d53a5c5b533a14643781d2d9c07f
eddio0141 <eddio0141@gmail.com>
2021-11-02 23:07:33 +0000

    using stderr for error messages

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,22 @@ use std::{env, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::from_args(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     // not sure if I can get around not living long enough problem
     let content = fs::read_to_string(&config.file_path).unwrap_or_else(|err| {
-        println!("Problem opening file: {}", err);
+        eprintln!("Problem opening file: {}", err);
         process::exit(1);
     });
     let mut hltas = HLTAS::from_str(&content).unwrap_or_else(|err| {
-        println!("Problem parsing hltas file: {}", err);
+        eprintln!("Problem parsing hltas file: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = hltas_cleaner::run(config, &mut hltas) {
-        println!("Error cleaning up, {}", e);
+        eprintln!("Error cleaning up, {}", e);
         process::exit(1);
     }
 }
