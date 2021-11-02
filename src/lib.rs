627d01e80eac675fcfc8f73af24511af8547e83f
eddio0141 <eddio0141@gmail.com>
2021-11-02 02:17:03 +0000

    progress

---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+use hltas::{HLTAS, types::{self, FrameBulk, Line}};
+use std::{error::Error, fs, num::NonZeroU32, slice::SliceIndex};
+
+pub fn run(config: Config, hltas: HLTAS) -> Result<(), Box<dyn Error>> {
+    let cleaned_up = no_dupe_framebulks(hltas);
+    println!("{:#?}", cleaned_up);
+
+    Ok(())
+}
+
+fn no_dupe_framebulks(hltas: HLTAS) -> HLTAS {
+    let prev_framebulk = {
+        let mut line_found: Option<(usize, &types::FrameBulk)> = None;
+        
+        for (i, line) in hltas.lines.iter().enumerate() {
+            if let Line::FrameBulk(bulk) = line {
+                line_found = Some((i, bulk));
+                break;
+            }
+        }
+
+        line_found
+    };
+
+    if prev_framebulk.is_none() {
+        return hltas;
+    }
+    
+    let first_framebulk_index = prev_framebulk.unwrap().0;
+    let prev_framebulk = prev_framebulk.unwrap().1;
+
+    let mut dupe_free_hltas = HLTAS::default();
+    dupe_free_hltas.properties = hltas.properties.clone();
+
+    for line in &hltas.lines[first_framebulk_index..] {
+        if let Line::FrameBulk(bulk) = line {
+            if bulk.action_keys == prev_framebulk.action_keys &&
+                bulk.movement_keys == prev_framebulk.movement_keys &&
+                bulk.action_keys == prev_framebulk.action_keys &&
+                bulk.pitch == prev_framebulk.pitch &&
+                bulk.console_command == prev_framebulk.console_command
+             {
+                // dupe found
+                // TODO, better way of doing this
+                let mut bulk_copy = bulk.clone();
+                let total_framecount = bulk.frame_count.get() + prev_framebulk.frame_count.get();
+                // the total came from nonzerou32 orignally so its fine
+                bulk_copy.frame_count = NonZeroU32::new(total_framecount).unwrap();
+                // i need to revise what * does
+                dupe_free_hltas.lines.push(Line::FrameBulk(bulk_copy));
+            }
+        } else {
+            dupe_free_hltas.lines.push(line.clone());
+        }
+    }
+
+    dupe_free_hltas
+}
+
+pub struct Config {
+    pub filename: String,
+    pub output_name: String,
+}
+
+impl Config {
+    pub fn from_args(args: &[String]) -> Result<Config, &str> {
+        let arg_count = 3;
+
+        if args.len() < arg_count {
+            return Err("not enough arguments");
+        }
+
+        let filename = args[1].clone();
+        let output_name = args[2].clone();
+    
+        Ok(Config { filename, output_name })
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use hltas::HLTAS;
+use hltas_cleaner::Config;
+use std::{env, fs, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::from_args(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    // not sure if I can get around not living long enough problem
+    let content = fs::read_to_string(&config.filename).unwrap_or_else(|err| {
+        println!("Problem opening file: {}", err);
+        process::exit(1);
+    });
+    let hltas = HLTAS::from_str(&content).unwrap_or_else(|err| {
+        println!("Problem parsing hltas file: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = hltas_cleaner::run(config, hltas) {
+        println!("Error cleaning up, {}", e);
+        process::exit(1);
+    }
+}
