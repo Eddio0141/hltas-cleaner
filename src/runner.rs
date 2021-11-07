fa179e78c6f25bbcf23a0ff3b868415a21f01a15
eddio0141 <eddio0141@gmail.com>
2021-11-07 01:53:51 +0000

    updated argument helper message

--- src/runner.rs
+++ src/runner.rs
@@ -26,7 +26,7 @@ impl Config {
 
         if args.len() < arg_count {
             // TODO better way of handling showing required arguments and environment variables
-            return Err("Usage: input_path output_path\nenvironment variables: NoBulkDupe (bool)");
+            return Err("\nUsage: input_path output_path\nenvironment variables: NoBulkDupe (bool)");
         }
 
         let filename = args[1].clone();
